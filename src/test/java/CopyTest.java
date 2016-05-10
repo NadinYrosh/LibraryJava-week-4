8434379a881bb20552f22c084fe06f53510d57fc
Daniel Wulff and Nadiya Yeroshkina <daniel@>
2016-05-10 16:25:42 -0700

    add save, find, override tests and methods

--- src/test/java/CopyTest.java
+++ src/test/java/CopyTest.java
@@ -10,7 +10,36 @@ public class CopyTest {
 
   @Test
   public void Copy_instantiatesCorrectly_true(){
-    Copy myCopy = new Copy();
+    Copy myCopy = new Copy("available", 1);
     assertEquals(true, myCopy instanceof Copy);
   }
+
+  @Test
+  public void Copy_instantiatesWithStatus_String(){
+    Copy myCopy = new Copy("available", 1);
+    assertEquals("available", myCopy.getStatus());
+  }
+
+  @Test
+  public void Copy_instantiatesWithBookId_Int(){
+    Copy myCopy = new Copy("available", 1);
+    assertEquals(1, myCopy.getBookId());
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Copy myCopy = new Copy("available", 1);
+    myCopy.save();
+    Copy savedCopy = Copy.all().get(0);
+    assertEquals(myCopy.getId(), savedCopy.getId());
+  }
+
+  @Test
+  public void find_findsCopyInDatabase_True() {
+    Copy myCopy = new Copy("available", 1);
+    myCopy.save();
+    Copy saveCopy = Copy.find(myCopy.getId());
+    assertTrue(myCopy.equals(saveCopy));
+  }
+
 }
