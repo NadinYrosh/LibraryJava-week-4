3b875c286975c3ed4f0c5dfcb1f1b6105781d892
Daniel Wulff and Nadiya Yeroshkina <nadiya@>
2016-05-10 10:44:45 -0700

    created author class tests and logic[

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,6 +1,10 @@
+import java.util.List;
+import org.sql2o.*;
+
 public class Author {
-  String first_name;
-  String last_name;
+  private String first_name;
+  private String last_name;
+  private int id;
 
   public Author (String firstName, String lastName) {
     this.first_name = firstName;
@@ -13,5 +17,69 @@ public class Author {
 
   public String getLastName() {
     return this.last_name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO authors (first_name, last_name) VALUES (:first, :last)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("first", this.first_name)
+        .addParameter("last", this.last_name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public void update(String newFirstName, String newLastName) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE authors SET first_name = :first, last_name = :last WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .addParameter("first", newFirstName)
+        .addParameter("last", newLastName)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM authors WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
+  public static Author find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM authors WHERE id = :id";
+      Author author = con.createQuery(sql)
+              .addParameter("id", id)
+              .executeAndFetchFirst(Author.class);
+      return author;
+    }
+  }
+
+  public static List<Author> all() {
+    String sql = "SELECT id, first_name, last_name FROM authors";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Author.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherAuthor) {
+    if (!(otherAuthor instanceof Author)) {
+      return false;
+    } else {
+      Author newAuthor =  (Author) otherAuthor;
+      return this.getFirstName().equals(newAuthor.getFirstName()) &&
+             this.getLastName().equals(newAuthor.getLastName()) &&
+             this.getId() == newAuthor.getId();
+    }
   }
 }
--- src/test/java/AuthorTest.java
+++ src/test/java/AuthorTest.java
@@ -1,6 +1,6 @@
 import org.junit.*;
 import org.sql2o.*;
-// import java.util.List;
+import java.util.List;
 // import java.util.ArrayList;
 
 import static org.junit.Assert.*;
@@ -28,5 +28,98 @@ public class AuthorTest {
     assertEquals("Smith", myAuthor.getLastName());
   }
 
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Author.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAreTheSame_true() {
+    Author firstAuthor = new Author("Dave", "Smith");
+    Author secondAuthor = new Author("Dave", "Smith");
+    assertTrue(firstAuthor.equals(secondAuthor));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase_true() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    assertTrue(Author.all().get(0).equals(myAuthor));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    Author savedAuthor = Author.all().get(0);
+    assertEquals(myAuthor.getId(), savedAuthor.getId());
+  }
+
+  @Test
+  public void find_findsAuthorsInDatabase_True() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    Author savedAuthor = Author.find(myAuthor.getId());
+    assertTrue(myAuthor.equals(savedAuthor));
+  }
+
+  @Test
+  public void update_updatesAuthorFirstName_true() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    myAuthor.update("Stephen", "Smith");
+    assertEquals("Stephen", Author.find(myAuthor.getId()).getFirstName());
+  }
+
+  @Test
+  public void update_updatesAuthorLastName_true() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    myAuthor.update("Dave", "Wells");
+    assertEquals("Wells", Author.find(myAuthor.getId()).getLastName());
+  }
+
+  @Test
+  public void delete_deletesAuthor_true() {
+    Author myAuthor = new Author("Dave", "Smith");
+    myAuthor.save();
+    int myAuthorId = myAuthor.getId();
+    myAuthor.delete();
+    assertEquals(null, Author.find(myAuthorId));
+  }
+
+  // @Test
+  // public void addCategory_addsCategoryToTask() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("Mow the lawn");
+  //   myTask.save();
+  //   myTask.addCategory(myCategory);
+  //   Category savedCategory = myTask.getCategories().get(0);
+  //   assertTrue(myCategory.equals(savedCategory));
+  // }
+  //
+  // @Test
+  // public void getCategories_returnsAllCategories_List() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("Mow the lawn");
+  //   myTask.save();
+  //   myTask.addCategory(myCategory);
+  //   List savedCategories = myTask.getCategories();
+  //   assertEquals(1, savedCategories.size());
+  // }
+  //
+  //
+  // @Test
+  // public void delete_deletesAllTasksAndCategoriesAssociations() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("Mow the lawn");
+  //   myTask.save();
+  //   myTask.addCategory(myCategory);
+  //   myTask.delete();
+  //   assertEquals(0, myCategory.getTasks().size());
+  // }
 
 }
